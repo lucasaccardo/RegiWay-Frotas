d2b03c8a7b9663346973af32be659ce61ba27f4d
B0cchi-Kun <andre.kenzo.n.n@gmail.com>
2026-05-19 09:19:53 -0300

    Update urls.py

--- apps/frotas/urls.py
+++ apps/frotas/urls.py
@@ -1,4 +1,10 @@
 # Funcao: rotas de clientes, veiculos, motoristas e consultas da frota.
 # Responsável: Kenzo.
+from django.contrib import admin
+from django.urls import include, path
 
-urlpatterns = []
+urlpatterns = [
+    path("admin/", admin.site.urls),
+    path("", include(("apps.accounts.urls", "two_factor"), namespace="two_factor")),
+    path("frotas/", include("apps.frotas.urls")),
+]
