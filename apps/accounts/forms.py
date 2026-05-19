d7ea5e187c019d701e4d1de280303a3cadd1381b
B0cchi-Kun <andre.kenzo.n.n@gmail.com>
2026-05-19 11:27:31 -0300

    Update forms.py

--- apps/accounts/forms.py
+++ apps/accounts/forms.py
@@ -1,2 +1,11 @@
 # Funcao: formularios de login, cadastro, senha, perfil e aceite de termos.
 # Responsável: Kenzo.
+
+from django.contrib.auth.forms import AuthenticationForm
+from django_recaptcha.fields import ReCaptchaField
+from django_recaptcha.widgets import ReCaptchaV2Checkbox
+
+
+class LoginPassoUmForm(AuthenticationForm):
+
+    captcha = ReCaptchaField(widget=ReCaptchaV2Checkbox)
