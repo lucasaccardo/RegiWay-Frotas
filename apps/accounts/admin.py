d322c3e428ee6766c828edf7d7c66ab92782db0b
B0cchi-Kun <andre.kenzo.n.n@gmail.com>
2026-05-20 03:51:43 -0300

    Update admin.py

--- apps/accounts/admin.py
+++ apps/accounts/admin.py
@@ -1,2 +1,13 @@
 # Funcao: registra modelos de accounts no Django Admin.
 # Responsável: Kenzo.
+
+from django.contrib import admin
+from .models import PerfilUsuario, TermoAceiteUsuario
+
+@admin.register(PerfilUsuario)
+class PerfilUsuarioAdmin(admin.ModelAdmin):
+    list_display = ('usuario', 'telefone', 'cargo')
+
+@admin.register(TermoAceiteUsuario)
+class TermoAceiteUsuarioAdmin(admin.ModelAdmin):
+    list_display = ('usuario', 'versao_termo', 'aceito_em')
