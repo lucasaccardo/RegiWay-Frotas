16998304bd21be653079d03fbe6fb8198fac64d4
kennyXP22 <kennyxp6@gmail.com>
2026-05-27 12:03:21 -0300

    [feat]Front-End provalvelmente totalmente feito mas precisa de analises

--- static/js/app.js
+++ static/js/app.js
@@ -1,2 +1,22 @@
 // Funcao: scripts globais de comportamento da interface.
 // Responsável: Kenny.
+document.addEventListener("DOMContentLoaded", () => {
+    console.log("RegiWay Frotas Front-End Inicializado com Sucesso!");
+
+    const alerts = document.querySelectorAll('.alert');
+    alerts.forEach(alert => {
+        setTimeout(() => {
+            alert.classList.add('fade-out');
+            setTimeout(() => {
+                alert.style.display = 'none';
+            }, 400);
+        }, 5000);
+    });
+    // Isso garante que os inputs do Kenzo/Lucas peguem seu CSS do app.css
+    const formInputs = document.querySelectorAll('input, select, textarea');
+    formInputs.forEach(input => {
+        if (input.type !== 'submit' && input.type !== 'checkbox') {
+            input.classList.add('form-control');
+        }
+    });
+});
